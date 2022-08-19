b04f313d16f4f6a3b4329cd3007396ea6886f3b5
Everett Harris <everettharr979@gmail.com>
2022-08-18 21:10:54 -0400

    added MySQL connection and datbase

--- CS_Beginner_MySQL/Form1.cs
+++ CS_Beginner_MySQL/Form1.cs
@@ -1,10 +1,31 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
 namespace CS_Beginner_MySQL
 {
     public partial class Form1 : Form
     {
+        MySqlConnection sqlconn = new MySqlConnection();
+        MySqlCommand sqlcmd = new MySqlCommand();
+        DataTable dt = new DataTable();
+        String sqlQuery;
+        MySqlDataAdapter Dta = new MySqlDataAdapter();
+        MySqlDataReader sqlRd;
+        
+        DataSet DS = new DataSet();
+
+        string server = "localhost";
+        string username = "root";
+        string password = "DREADhead2638!";
+        string database = "membership";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
